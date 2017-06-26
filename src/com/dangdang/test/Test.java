f8cb22480578cbd33aef062844c3ad5ec2276149
renzaitianya <renzaitianya2006@163.com>
2017-06-26 09:47:17 +0800

    215351235

---
+++ src/com/dangdang/test/Test.java
@@ -0,0 +1,21 @@
+package com.dangdang.test;
+
+public class Test {
+
+	public void test1() {
+		System.out.println("test1执行");
+		return;
+	}
+
+	public void test2() {
+		System.out.println("test2执行");
+	}
+
+	public static void main(String[] args) {
+		int a = 1;
+		Test test = new Test();
+		test.test1();
+		test.test2();
+	}
+
+}
